ec73b31f54c64e296f41997351f1ebf8f19b3703
cbsolimantech <78085877+cbsolimantech@users.noreply.github.com>
2021-01-27 21:21:52 +0800

    Minor formatting update

--- FunctionApp1/LetterComposer.cs
+++ FunctionApp1/LetterComposer.cs
@@ -25,8 +25,7 @@ namespace FunctionApp1
                 //Parse flattery list into comma separated string
                 //Populate Header with salutation comma separated string and "Mother"
                 //Heading=Greeting
-                Heading = new CultureInfo("en-us", false).TextInfo.ToTitleCase(string.Join(", ", _messageToMom.Flattery)) +
-                    " Mother, " + _messageToMom.Greeting,
+                Heading = $"{new CultureInfo("en-us", false).TextInfo.ToTitleCase(string.Join(", ", _messageToMom.Flattery))} Mother, {_messageToMom.Greeting}",
 
                 //Body:"Really need help: I need $5523.23 by December 12,2020"
                 Body = $"Really need help: I need {_messageToMom.HowMuch:c} by {_messageToMom.HowSoon.Value:MMMM dd, yyyy}",
